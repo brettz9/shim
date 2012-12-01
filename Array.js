29e06d5c37ca11d3e4f39e2ab105f7acdf44001d
Brett Zamir <brettz9@yahoo.com>
2012-12-01 22:52:16 +0800

    Clarify comment

--- Array.js
+++ Array.js
@@ -5,7 +5,8 @@
 define(['generics', 'shim!Array.prototype.forEach'], function (generics) {
     'use strict';
     var ret = {};
-    // We can also build the array of methods with the following, but, it is non-shimable
+    // We can also build the array of methods with the following, but the 
+    //   getOwnPropertyNames() method is non-shimable:
     // Object.getOwnPropertyNames(Array).filter(function (methodName) {return typeof Array[methodName] === 'function'});
     [
         'join', 'reverse', 'sort', 'push', 'pop', 'shift', 'unshift',
