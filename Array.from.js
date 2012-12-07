0b89322a7d9868fabb1445ca7082a43ea5764d22
Brett Zamir <brettz9@yahoo.com>
2012-12-07 23:14:08 +0800

    Remove dependency

--- Array.from.js
+++ Array.from.js
@@ -1,5 +1,5 @@
 /*globals define*/
-define(['shim!Array.prototype.map'], function () {
+define(function () {
     'use strict';
     return function (object) {
         return [].slice.call(object);
