6f4d7caae4e8c7b538749b90920e6357fcff85c5
Brett Zamir <brettz9@yahoo.com>
2013-08-11 15:46:30 +0800

    amdefine info

--- demos/nodeTest.js
+++ demos/nodeTest.js
@@ -2,6 +2,7 @@
 
 // Set up regretttably-necessary boilerplate at top of pipeline (though could be in separate required config file) to set up AMD-implementation-independent and environment-independent requirer variable
 if (typeof window === 'undefined') {
+    // global.define = require('amdefine')(module); // Unfortunately we can't do this to avoid module definition boilerplate as it depends on the specific "module"
     global.requirer = require('requirejs');
     requirer.config({
         paths: {writeBr: 'writeBrNode'}, // Todo: Find some other way to look for environment-specific versions? Might RequireJS provide a way for preference to be given, depending on the environment, to a specific folder (e.g., for Node-specific, SDK-specific, browser-specific, AsYouWish/priv!, etc. versions) and if not found there, to default; without needing to hard-code folder prefixes for each module that config.paths can use to do rerouting. (If not, one could do "envt/" prefix to indicate a module that will vary with the environment, but this asks for more typing.)
