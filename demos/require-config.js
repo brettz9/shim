9920f9f86e81bc638f08dc6c5b2d127a7514881e
Brett Zamir <brettz9@yahoo.com>
2013-08-13 13:52:28 +0800

    Add detection to require-config.js, fix shims.html and ArrayGenerics to use new approach/path

--- demos/require-config.js
+++ demos/require-config.js
@@ -9,13 +9,29 @@ require.config({
         shim: { // IMPORTANT: This "shim" plugin property must be within the "config" property, as distinguished from the "shims" property at the top level for shimming non-AMD modules
             // The following work when the main "shim" is at the root of the web root
             pathDepth: 'one', // Namespaces one folder deep based on first part of file name preceding the first "." (though the default behavior still expects the file to contain the folder name: e.g., require('shim!Array.prototype.map'); would look for "Array/Array.prototype.map.js" relative to any base paths)
-            baseUrl: '../shims' // Relative to baseUrl (which defaults to "./" of the executing HTML file or data-main on the script tag if present)
-            // baseUrl: '/shim/shims'
-            // baseUrl: 'http://127.0.0.1/shim/shims'
+            baseUrl: '../shims', // Relative to baseUrl (which defaults to "./" of the executing HTML file or data-main on the script tag if present)
+            // baseUrl: '/shim/shims',
+            // baseUrl: 'http://127.0.0.1/shim/shims',
 
             // The following work when the main "shim" is a file URL:
-            // baseUrl: '../shims' // Relative to baseUrl (which defaults to "./" of the executing HTML file or data-main on the script tag if present)
-            // baseUrl: 'file://d:/wamp/www/shim/shims' // On my system
+            // baseUrl: '../shims', // Relative to baseUrl (which defaults to "./" of the executing HTML file or data-main on the script tag if present)
+            // baseUrl: 'file://d:/wamp/www/shim/shims', // On my system
+            detect: {
+                Array: {
+                    // 'prototype': 'map' // Can also use a string to indicate
+                    // method to check to allow all to be added (or require
+                    // shims!Array.prototype!map, though probably better
+                    // to avoid polluting the require code)
+                    prototype: function (obj) {
+                        'use strict';
+                        return obj.map;
+                    },
+                    // For objects already holding properties (as above), we
+                    //   can use "detect" also with a string, boolean, array, or
+                    //   function
+                    detect: ['slice', 'map']
+                }
+            }
         }
     }
 });
